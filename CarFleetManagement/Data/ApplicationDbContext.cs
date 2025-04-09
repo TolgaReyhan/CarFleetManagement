6b62c9a02637908a38d9c125231a8b31a3c7ef7e
Student <student@abv.bg>
2025-04-09 10:29:36 +0300

    asdasdsa

---
+++ CarFleetManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CarFleetManagement.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarFleetManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.Migrate();
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<FuelExpense> FuelExpenses { get; set; }
+        public DbSet<InsuranceExpense> InsuranceExpenses { get; set; }
+        public DbSet<RepairExpense> RepairExpenses { get; set; }
+        public DbSet<MonthlyReport> MonthlyReports { get; set; }
+    }
+}
---
+++ CarFleetManagement/Models/FuelExpenseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarFleetManagement.Models
+{
+    public class FuelExpenseViewModel
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public string CarModel { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Liters { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
